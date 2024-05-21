e2485e9cae4076a38ca32608aa5e53a2d8ac91a6
Joakim Nohlgård <joakim@nohlgard.se>
2024-05-21 11:28:59 +0200

    Avoid error on undefined variables in variable checks
    Fixes errors when starting without the variable set:
    
    /usr/bin/set_init_password.sh: line 6: PULP_DEFAULT_ADMIN_PASSWORD: unbound variable

--- images/assets/set_init_password.sh
+++ images/assets/set_init_password.sh
@@ -3,21 +3,21 @@
 /usr/bin/wait_on_postgres.py
 /usr/bin/wait_on_database_migrations.sh
 
-if [[ -n "$PULP_DEFAULT_ADMIN_PASSWORD" ]]
+if [ -n "${PULP_DEFAULT_ADMIN_PASSWORD:+set}" ]
 then
   PASSWORD_SET=$(/usr/local/bin/pulpcore-manager shell -c "from django.contrib.auth import get_user_model; print(get_user_model().objects.filter(username=\"admin\").exists())")
-  if [ "$PASSWORD_SET" = "False" ]
+  if [ "${PASSWORD_SET}" = "False" ]
   then
     /usr/local/bin/pulpcore-manager reset-admin-password --password "${PULP_DEFAULT_ADMIN_PASSWORD}"
   fi
 else
   ADMIN_PASSWORD_FILE=/etc/pulp/pulp-admin-password
-  if [[ -f "$ADMIN_PASSWORD_FILE" ]]; then
+  if [ -f "${ADMIN_PASSWORD_FILE}" ]; then
      echo "pulp admin can be initialized."
      PULP_ADMIN_PASSWORD=$(cat $ADMIN_PASSWORD_FILE)
   fi
 
-  if [ -n "${PULP_ADMIN_PASSWORD}" ]; then
+  if [ -n "${PULP_ADMIN_PASSWORD:+set}" ]; then
       /usr/local/bin/pulpcore-manager reset-admin-password --password "${PULP_ADMIN_PASSWORD}"
   fi
 fi
